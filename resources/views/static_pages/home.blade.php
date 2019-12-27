21f0dd9c5eac7a7853e5177bba058053b42082cd
yangyuan <243882446@163.com>
2019-12-27 09:36:00 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是<a href="">个性微博</a>主页
+        </p>
+        <p>
+          开始探索之旅吧
+        </p>
+        <p>
+          <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
