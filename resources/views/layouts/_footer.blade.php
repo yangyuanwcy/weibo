528beade11f921e1d0f7ed5d41e8289dacbeb03c
yangyuan <243882446@163.com>
2020-01-09 03:36:46 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="https://learnku.com/laravel/courses" target="_blank">
+    我是底部视图
+  </a>
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
