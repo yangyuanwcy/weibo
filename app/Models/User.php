4b7edeb05ca32b9f27d33b171a09ba730327cd0e
yangyuan <243882446@163.com>
2020-01-09 07:51:00 +0000

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
