fd90fe65a5fa4ab3df7d8dcbd9a6d834ff59a2d3
yangyuan <243882446@163.com>
2020-01-10 07:08:24 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,6 +23,12 @@ class UsersController extends Controller
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
-        return;
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+        session()->flash('success','恭喜注册成功，开始您的新旅程吧！');
+        return redirect()->route('users.show',[$user]);
     }
 }
