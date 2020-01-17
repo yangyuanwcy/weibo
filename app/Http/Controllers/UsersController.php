2cc5de4cf4ba92cdf77847e6b2e75e74c91e18e5
yangyuan <243882446@163.com>
2020-01-17 03:01:19 +0000

    移除邮件发送from选项并配置邮件环境

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -81,13 +81,11 @@ class UsersController extends Controller
     {
         $view="emails.confirm";
         $data=compact('user');
-        $from='yangyuanwcy@gmail.com';
-        $name='Young';
         $to=$user->email;
         $subject="感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view,$data,function ($message) use ($from,$name,$to,$subject){
-            $message->from($from,$name)->to($to)->subject($subject);
+        Mail::send($view,$data,function ($message) use ($to,$subject){
+            $message->to($to)->subject($subject);
         });
     }
     public function confirmEmail($token)
